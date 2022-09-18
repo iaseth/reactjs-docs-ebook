5aa2ee9cb486f03cddb0ea65ed648fa43cbf3249
Ankur Seth <iankurseth@gmail.com>
2022-09-18 23:08:57 +0530

    Added empty scripts for generator

---
+++ generate_ebook.py
@@ -0,0 +1,9 @@
+from pyreactorg import ReactOrgApp
+
+
+def main():
+	app = ReactOrgApp()
+	app.print()
+
+if __name__ == '__main__':
+	main()
---
+++ pyreactorg/app.py
@@ -0,0 +1,10 @@
+
+
+
+
+class ReactOrgApp:
+	def __init__(self):
+		pass
+
+	def print(self):
+		print(f"ReactOrgApp says hello!")
